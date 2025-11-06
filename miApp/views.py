7996c62487ddf2c300a6d8960aca374e76ef36bf
Juan Pablo Moreno <juan.moreno60@inacapmail.cl>
2025-11-06 15:06:33 -0300

    mi primer commit

---
+++ miApp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Tarea
+from .forms import TareaForm
+
+def home(request):
+    if request.method == "POST":
+        form = TareaForm(request.POST)
+        print("------- FORMULARIO CON DATOS ----------")
+        print(form)
+        if form.is_valid():
+            form.save()
+    form = TareaForm()
+    #print(form)
+    tareas = Tarea.objects.all() #-> Lista de objetos
+    context = {'tareas':tareas,'form':form}
+    return render(request,'home.html',context)
